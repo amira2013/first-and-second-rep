c3251211ee6f96bdb8aa98368e4aafbf3072288a
amira2013 <drisschima42@gmail.com>
2020-10-26 21:38:28 -0400

    first and second

---
+++ FirstPraogram.java
@@ -0,0 +1,12 @@
+package com.syntax.class1;
+
+public class FirstPraogram {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello world");
+		System.out.println("i am a programmer");
+
+	}
+
+}
---
+++ MySecondProgram.java
@@ -0,0 +1,12 @@
+package com.syntax.class1;
+
+public class MySecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		
+		System.out.println("This is my second java program");
+
+	}
+
+}
